8f02e0a725cc9e53ee8826901e948e0b66d2d769
Benno <57860196+bennoinbeta@users.noreply.github.com>
2024-08-16 16:13:37 +0200

    #1 removed popup

--- apps/sfy-chrome-extension/entrypoints/appstore.content/App.tsx
+++ apps/sfy-chrome-extension/entrypoints/appstore.content/App.tsx
@@ -6,11 +6,31 @@ import {
 	DialogHeader,
 	DialogTitle
 } from '@repo/ui';
+import React from 'react';
 
 import { Launcher } from './components';
 import { SHADOW_ROOT_NAME } from './index';
 
 export const App: React.FC = () => {
+	const onClick = React.useCallback(async () => {
+		const res = await browser.runtime.sendMessage('ping');
+
+		console.log(res); // "pong"
+
+		// const shopifyResult = await fetch(
+		// 	'https://apps.shopify.com/search?page=1&q=review',
+		// 	{
+		// 		headers: {
+		// 			'Accept': 'text/html, application/xhtml+xml',
+		// 			'Turbo-Frame': 'search_page'
+		// 		}
+		// 	}
+		// );
+		// const shopifyHtml = await shopifyResult.text();
+
+		// console.log('Hello background!', { id: browser.runtime.id, shopifyHtml });
+	}, []);
+
 	return (
 		<div className="z-[9999]">
 			<Dialog>
@@ -27,24 +47,7 @@ export const App: React.FC = () => {
 					<DialogHeader>
 						<DialogTitle>Hello world</DialogTitle>
 						<DialogDescription>This is a test</DialogDescription>
-						<Button
-							onClick={async () => {
-								const shopifyResult = await fetch(
-									'https://apps.shopify.com/search?page=1&q=review',
-									{
-										headers: {
-											'Accept': 'text/html, application/xhtml+xml',
-											'Turbo-Frame': 'search_page'
-										}
-									}
-								);
-								const shopifyHtml = await shopifyResult.text();
-
-								console.log('Hello background!', { id: browser.runtime.id, shopifyHtml });
-							}}
-						>
-							Submit
-						</Button>
+						<Button onClick={onClick}>Submit</Button>
 					</DialogHeader>
 				</DialogContent>
 			</Dialog>
