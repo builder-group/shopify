d0d6fad83f44a295e47a9b03f42cb15772af94d4
Benno <57860196+bennoinbeta@users.noreply.github.com>
2024-08-20 18:07:05 +0200

    #1 completed poc

--- packages/ui/src/components/icon/index.ts
+++ packages/ui/src/components/icon/index.ts
@@ -1,5 +1,6 @@
 // Custom Icons
 export * from './LogoIcon';
+export * from './SpinnerIcon';
 
 // Third Party Icons
 export { DotsVerticalIcon } from '@radix-ui/react-icons';
--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,4 +1,5 @@
 export * from './debug';
+export * from './display';
 export * from './icon';
 export * from './input';
 export * from './layout';
