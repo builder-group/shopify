e246c8d8e224c443687ca9f86f9f63972bdb9c9b
Benno <57860196+bennoinbeta@users.noreply.github.com>
2024-08-25 15:29:35 +0200

    #3 checkout-ui in playground

---
+++ apps/sfy-eu-blocks-app/checkout-ui-extension/src/CheckoutCartLineItem.tsx
@@ -0,0 +1,10 @@
+import { reactExtension, Text, useCartLineTarget } from '@shopify/ui-extensions-react/checkout';
+
+export default reactExtension('purchase.checkout.cart-line-item.render-after', () => <Extension />);
+
+function Extension() {
+	const {
+		merchandise: { title }
+	} = useCartLineTarget();
+	return <Text>Checkout - Line item title: {title}</Text>;
+}
---
+++ apps/sfy-eu-blocks-app/checkout-ui-extension/src/OrderSummaryCartLineItem.tsx
@@ -0,0 +1,11 @@
+import { reactExtension, Text, useTarget } from '@shopify/ui-extensions-react/customer-account';
+
+export default reactExtension('customer-account.order-status.cart-line-item.render-after', () => (
+	<Extension />
+));
+function Extension() {
+	const {
+		merchandise: { title }
+	} = useTarget();
+	return <Text>Summary - Line item title: {title}</Text>;
+}
